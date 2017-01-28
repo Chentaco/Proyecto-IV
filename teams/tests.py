630de706458901bc50d441278cc2599b2bb2c574
Chentaco <chenteuni@gmail.com>
2017-01-28 16:48:59 +0100

    Fixing test

--- teams/tests.py
+++ teams/tests.py
@@ -30,8 +30,8 @@ class JugadorTestCase(TestCase):
 
 	def test_jugador_pertenece_team(self):
 		"""Un jugador pertenece a un team"""
-		Iniesta = Jugador.objects.get(nick="Andres", nombre="Iniesta")
-		Biraghi = Jugador.objects.get(nick="Cristiano", nombre="Biraghi")
+		Iniesta = Jugador.objects.get(nick="Andres", name="Iniesta")
+		Biraghi = Jugador.objects.get(nick="Cristiano", name="Biraghi")
 		
 		pMaceta = Team.objects.get(nombre="La Maceta")
 		pTercerTiempo = Team.objects.get(nombre="El Tercer Tiempo")
@@ -46,7 +46,7 @@ class RutasTeamsJSON(APITestCase):
 		Team.objects.create(nombre="El Tercer Tiempo", ciudad="Granada")
 		Team.objects.create(nombre="La Maceta", ciudad="Peligros")
 
-		response = self.client.get('/match/team/json/teams/')
+		response = self.client.get('/match/teams/json/teams/')
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(response['Content-Type'], 'application/json')
 
@@ -59,7 +59,7 @@ class RutasTeamsJSON(APITestCase):
 		
 		teams = Team.objects.values_list('id',flat=True)
 		for i in teams:
-			response = self.client.get('/match/team/json/teams/'+str(i)+'/')
+			response = self.client.get('/match/teams/json/teams/'+str(i)+'/')
 			self.assertEqual(response.status_code, status.HTTP_200_OK)
 			self.assertEqual(response['Content-Type'], 'application/json')
 			print("Ruta /teams/" + str(i) + "/ consultada correctamente")
